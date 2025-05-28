428662a6f3d979ed26249b7eee89054d8ef27fb0
Negin Raoof <neginmr@utexas.edu>
2025-05-28 14:31:35 -0700

    Update eval_instruct.py

--- eval/chat_benchmarks/RepoBench/eval_instruct.py
+++ eval/chat_benchmarks/RepoBench/eval_instruct.py
@@ -91,7 +91,7 @@ class RepoBenchmark(BaseBenchmark):
                             example,
                             (
                                 prompt,
-                                {"max_gen_toks": 128, "temperature": 0.2, "top_p": 0.95, "do_sample": True},
+                                {"max_new_tokens": self.max_tokens, "temperature": 0.2, "top_p": 0.95, "do_sample": True},
                             ),
                             idx,
                         )
